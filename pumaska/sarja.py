bfbcf95f75bab28c027b2b4dfbfde04bba676179
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2020-03-11 13:55:12 +0200

    Korjattu `ugettext_lazy` -> `gettext_lazy`

--- pumaska/sarja.py
+++ pumaska/sarja.py
@@ -6,7 +6,7 @@ from django.db.models import ProtectedError
 from django import forms
 from django.template import loader
 from django.utils.functional import cached_property
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 def lisaa_lomakesarja(
