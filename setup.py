bc4e7ed1b68d357796d03a1bfda1a1042011fbf1
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2023-08-28 14:08:40 +0300

    Päivitetty versiointijärjestelmä

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  setup_requires='git-versiointi',
+  setup_requires='git-versiointi>=1.6rc3',
   name='django-pumaska',
   description='Sisäkkäisten lomakkeiden ja -sarjojen käsittely',
   url='https://github.com/an7oine/django-pumaska',
